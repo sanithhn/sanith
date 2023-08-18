4fd11621437a5997ca6112f90f915481593cfdcc
sanith <darshandc202@gmail.com>
2023-08-18 12:37:24 +0530

    change4

--- python3.py
+++ python3.py
@@ -16,6 +16,12 @@ lst = [1,2,3,4,5,6,7,8]
 f=list(map(lambda x : x+5,lst))
 print(f)
 
+a=[1,1,1,1]
+b=[1,2,3,4]
+c=[1,1,1,1]
+d=list(map(lambda e,f,g : e+f+g,a,b,c))
+print(d)
+
 #FILTER
 lst = [1,2,3,4,5,6,7,8]
 k=list(filter(lambda v : v%2,lst))
@@ -25,8 +31,18 @@ lst = [1,2,3,4,5,6,7,8]
 k=list(filter(lambda v : v>7,lst))
 print(k)
 
+lst=["mango","apple","orange","kiwi"]
+d=list(filter(lambda v : "g" in v ,lst))
+print(d)
+
 #reduce
 lst=[1,2,3,4,5,6,7,8,9]
 from functools import reduce
 p=reduce(lambda v,d : v+d,lst)
 print(p)
+
+a=[1,1,1,1]
+b=[1,2,3,4]
+c=[1,1,1,1]
+d=list(map(lambda e,f,g : e+f+g,a,b,c))
+print(d)
